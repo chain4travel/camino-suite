42490a524c598367a18bb0539377b234c6b3799e
Ayagoumi <agoumi94@gmail.com>
2022-11-30 00:35:40 +0100

    fix: update public path for wallet in webpack config

--- wallet/webpack.config.js
+++ wallet/webpack.config.js
@@ -6,7 +6,7 @@ const path = require("path");
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:3003/",
+    publicPath: "https://playground.suite-wallet.camino.foundation/",
   },
 
   devServer: {
