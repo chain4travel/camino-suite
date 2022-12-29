e684e4628be9e6ac148436d7ee3087ac6536c4cb
Daniel Lopez <danilocha1@gmail.com>
2022-12-29 17:33:14 -0500

    add date picker in charts

--- blockexplorer/src/app/components/Charts/index.tsx
+++ blockexplorer/src/app/components/Charts/index.tsx
@@ -1,23 +1,26 @@
-import React, { useEffect, Fragment, useState } from 'react';
+import React, { Fragment, useState } from 'react';
 // import { useAppDispatch, useAppSelector } from 'store/configureStore';
-import { typeMeter as typesMeter } from '../../pages/Statistics/typeMeter';
-import { Status } from "types";
+import { Status } from 'types';
 import CircularProgress from '@mui/material/CircularProgress';
 import LinearMeter from './LinearMeter';
 import IconButton from '@mui/material/IconButton';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquareArrowUpRight, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSquareArrowUpRight,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import useWidth from 'app/hooks/useWidth';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-const Charts = ({
-  darkMode, titleText
-}) => {
-
+const Charts = ({ darkMode, titleText }) => {
   const { isDesktop } = useWidth();
 
   const [openModal, setOpenModal] = useState(false);
+  const [startDate, setStartDate] = useState(new Date('2022/12/29'));
+  const [endDate, setEndDate] = useState(new Date('2022/12/29'));
 
   //   const dispatch = useAppDispatch();
 
@@ -33,51 +36,81 @@ const Charts = ({
 
   return (
     <Fragment>
-
-
-      {loader == Status.LOADING ? <>
-        <div style={{ textAlign: 'center' }}>
-          <CircularProgress color="secondary" />
-        </div>
-      </> : <>
-
-        <Modal
-          open={openModal}
-          onClose={(e) => {
-            setOpenModal(false);
-          }}
-          sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-          disableScrollLock={true}
-        >
-          <Box
-            sx={{
-              backgroundColor: "primary.main",
-              borderRadius: "7px",
-              padding: "1.5rem",
-              minWidth: isDesktop ? '1300px' : '0px'
+      {loader === Status.LOADING ? (
+        <>
+          <div style={{ textAlign: 'center' }}>
+            <CircularProgress color="secondary" />
+          </div>
+        </>
+      ) : (
+        <>
+          <Modal
+            open={openModal}
+            onClose={e => {
+              setOpenModal(false);
             }}
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            disableScrollLock={true}
           >
-            <Fragment>
-              <div style={{ float: 'right' }}>
-                <IconButton color="info" component="label" onClick={() => setOpenModal(false)}>
-                  <FontAwesomeIcon icon={faXmark} />
-                </IconButton>
-              </div>
-              <LinearMeter darkMode={darkMode} titleText={titleText} />
-            </Fragment>
-          </Box>
-        </Modal>
+            <Box
+              sx={{
+                backgroundColor: 'primary.main',
+                borderRadius: '7px',
+                padding: '1.5rem',
+                minWidth: isDesktop ? '1300px' : '0px',
+              }}
+            >
+              <Fragment>
+                <div style={{ float: 'right' }}>
+                  <IconButton
+                    color="info"
+                    component="label"
+                    onClick={() => setOpenModal(false)}
+                  >
+                    <FontAwesomeIcon icon={faXmark} />
+                  </IconButton>
+                </div>
+                <>
+                  <DatePicker
+                    selected={startDate}
+                    onChange={date => setStartDate(date)}
+                    selectsStart
+                    startDate={startDate}
+                    endDate={endDate}
+                  />
+                  <DatePicker
+                    selected={endDate}
+                    onChange={date => setEndDate(date)}
+                    selectsEnd
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={startDate}
+                  />
+                </>
+                <LinearMeter darkMode={darkMode} titleText={titleText} />
+              </Fragment>
+            </Box>
+          </Modal>
 
-        <div style={{ float: 'right' }}>
-          <IconButton color="info" component="label" onClick={() => setOpenModal(true)}>
-            <FontAwesomeIcon icon={faSquareArrowUpRight} />
-          </IconButton>
-        </div>
+          <div style={{ float: 'right' }}>
+            <IconButton
+              color="info"
+              component="label"
+              onClick={() => setOpenModal(true)}
+            >
+              <FontAwesomeIcon icon={faSquareArrowUpRight} />
+            </IconButton>
+          </div>
 
-        <LinearMeter darkMode={darkMode} titleText={titleText} />
-      </>}
+          <LinearMeter darkMode={darkMode} titleText={titleText} />
+        </>
+      )}
     </Fragment>
   );
-}
+};
 
 export default Charts;
