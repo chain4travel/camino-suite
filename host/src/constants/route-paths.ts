9ac754f70de28e88844689249b081b4db0ba3cfb
Ayagoumi <agoumi94@gmail.com>
2023-01-14 23:35:11 +0100

    update design of the host app

---
+++ host/src/constants/route-paths.ts
@@ -0,0 +1,21 @@
+export const DOCS = "https://docs.camino.foundation/";
+
+export const DISCORD = "https://discord.gg/K5THjAweFB";
+
+export const TWITTER = "https://twitter.com/CaminoFndtn";
+
+export const TELEGRAM = "https://t.me/caminochain";
+
+export const MEDIUM = "https://medium.com/@caminofoundation";
+
+export const GITHUB = "https://github.com/chain4travel/camino-docs";
+
+export const WALLET = "/wallet";
+
+export const BASE_PATH = "/explorer";
+
+export const CCHAIN = BASE_PATH + "/c-chain";
+
+export const XCHAIN = BASE_PATH + "/x-chain";
+
+export const PCHAIN = BASE_PATH + "/p-chain";
