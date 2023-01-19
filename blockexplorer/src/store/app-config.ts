b53ac48b3de86337f1c8c9b159c079428628ccdd
aeddaqqa <achrafeddaqqaq@gmail.com>
2023-01-19 02:38:41 +0100

    fix network switch

--- blockexplorer/src/store/app-config.ts
+++ blockexplorer/src/store/app-config.ts
@@ -1,24 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { getChains } from "api";
-import { RootState } from "store/configureStore";
-import { Status } from "types";
-import { Chain, Network } from "types/store";
+import { createSlice } from '@reduxjs/toolkit';
+import { getChains } from 'api';
+import { RootState } from 'store/configureStore';
+import { Status } from 'types';
+import { Chain, Network } from 'types/store';
 
 const getNetworkFromLocalStorage = () => {
-  let activeNetwork = localStorage.getItem("activeNetwork");
-  if (activeNetwork === "undefined") {
-    localStorage.setItem("activeNetwork", JSON.stringify("camino-testnet"));
-    return "camino-testnet";
+  let activeNetwork = localStorage.getItem('activeNetwork');
+  if (activeNetwork === 'undefined') {
+    localStorage.setItem('activeNetwork', JSON.stringify('camino-testnet'));
+    return 'camino-testnet';
   }
   if (activeNetwork) return JSON.parse(activeNetwork);
   else {
-    localStorage.setItem("activeNetwork", JSON.stringify("camino-testnet"));
-    return "camino-testnet";
+    localStorage.setItem('activeNetwork', JSON.stringify('camino-testnet'));
+    return 'camino-testnet';
   }
 };
 
 const getCustomNetworksFromLocalStorage = () => {
-  let customNetworks = localStorage.getItem("customNetworks");
+  let customNetworks = localStorage.getItem('customNetworks');
   if (customNetworks) {
     return JSON.parse(customNetworks);
   }
@@ -31,7 +31,7 @@ interface chainArgs {
 }
 interface initialStateAppConfigType {
   activeNetwork?: string;
-  networks: Network[];
+  networks: any;
   chains: chainArgs[];
   activeTheme: string;
   status: Status;
@@ -41,59 +41,52 @@ let initialState: initialStateAppConfigType = {
   activeNetwork: getNetworkFromLocalStorage(),
   networks: [
     {
-      id: "camino-testnet",
-      displayName: "Columbus",
-      protocol: "https",
-      host: "columbus.camino.foundation",
-      magellanAddress: "https://magellan.columbus.camino.foundation",
+      id: 'camino-testnet',
+      displayName: 'Columbus',
+      protocol: 'https',
+      host: 'columbus.camino.foundation',
+      magellanAddress: 'https://magellan.columbus.camino.foundation',
       port: 443,
       predefined: true,
     },
     {
-      id: "mainnet-testnet",
-      displayName: "Mainnet",
-      protocol: "https",
-      host: "columbus.camino.foundation",
-      magellanAddress: "https://magellan.columbus.camino.foundation",
+      id: 'mainnet-testnet',
+      displayName: 'Mainnet',
+      protocol: 'https',
+      host: 'columbus.camino.foundation',
+      magellanAddress: 'https://magellan.columbus.camino.foundation',
       port: 443,
       predefined: true,
     },
     ...(getCustomNetworksFromLocalStorage() as Network[]),
   ],
   chains: [],
-  activeTheme: "dark",
+  activeTheme: 'dark',
   status: Status.IDLE,
 };
 
 const appConfigSlice = createSlice({
-  name: "appConfig",
+  name: 'appConfig',
   initialState,
   reducers: {
     changeNetwork: (state, action) => {
-      let active = state.networks.find(
-        (item) => item.displayName === action.payload
-      );
-      state.activeNetwork = active?.id;
-      localStorage.setItem("activeNetwork", JSON.stringify(active?.id));
-    },
-    addCustomNetwork: (state, action) => {
-      state.networks = [...state.networks, action.payload];
-    },
-    removeCustomNetwork: (state, action) => {
-      state.networks = state.networks.filter(
-        (item) => item.id !== action.payload
+      state.activeNetwork = state.networks.find(
+        item => item.id === action.payload.id,
       );
     },
-    resetChains: (state) => {
+    resetChains: state => {
       state.chains = [];
     },
     changeTheme: (state, { payload }) => {
       state.activeTheme = payload;
     },
+    updateNetworks: (state, { payload }) => {
+      state.networks = payload;
+    },
   },
   extraReducers(builder) {
     builder
-      .addCase(getChains.pending, (state) => {
+      .addCase(getChains.pending, state => {
         state.status = Status.LOADING;
       })
       .addCase(getChains.fulfilled, (state, { payload }) => {
@@ -103,7 +96,7 @@ const appConfigSlice = createSlice({
         });
         state.status = Status.SUCCEEDED;
       })
-      .addCase(getChains.rejected, (state) => {
+      .addCase(getChains.rejected, state => {
         state.status = Status.FAILED;
       });
   },
@@ -117,19 +110,14 @@ export const selectMagellanAddress = (state: RootState) => {
   // state.appConfig.networks.filter()
   let networks = state.appConfig;
   let activeNetwork = networks.networks.find(
-    (element) => element.id === networks.activeNetwork
+    element => element.id === networks.activeNetwork,
   );
-  return activeNetwork?.magellanAddress;
+  return activeNetwork?.explorerUrl;
 };
 
-// Select All networks
-export const getNetworks = (state: RootState) => state.appConfig.networks;
 // Select All chains
 export const selectAllChains = (state: RootState) => state.appConfig.chains;
-// Select All chain
-// export const selectChain = (state: RootState, location: string) => {
-//   return state.appConfig.chains;
-// };
+
 // Select Network Status
 export const selectNetworkStatus = (state: RootState) => state.appConfig.status;
 // Select Theme
@@ -137,9 +125,9 @@ export const selectedTheme = (state: RootState) => state.appConfig.activeTheme;
 
 export const {
   changeNetwork,
-  addCustomNetwork,
-  removeCustomNetwork,
+
   resetChains,
   changeTheme,
+  updateNetworks,
 } = appConfigSlice.actions;
 export default appConfigSlice.reducer;
