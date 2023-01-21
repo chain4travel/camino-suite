e4d8140edd9765593c901e8c1312ddb1987ce1f2
Ayoub Agoumi <agoumi94@gmail.com>
2023-01-21 11:23:04 +0100

    Optimized bundle size using split chunks and cache groups (#32)

--- wallet/webpack.prod.js
+++ wallet/webpack.prod.js
@@ -13,6 +13,29 @@ module.exports = merge(common, {
         chunkFilename: 'js/[name].[fullhash:8].' + timestamp + '.js',
     },
 
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            minSize: 0,
+            maxSize: 512000,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true,
+                },
+            },
+        },
+    },
+
     performance: {
         hints: false,
         maxEntrypointSize: 512000,
