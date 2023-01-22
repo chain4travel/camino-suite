020958f3a48e8da9d75b2c1d9d3414a66e2c965f
Ayagoumi <agoumi94@gmail.com>
2023-01-22 23:45:17 +0100

    Fixed request for retrieving validators

--- blockexplorer/src/store/validatorsSlice/utils.ts
+++ blockexplorer/src/store/validatorsSlice/utils.ts
@@ -1,23 +1,20 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import { store } from "../../App.tsx";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { store } from '../../App.tsx';
 
-export const loadValidators = createAsyncThunk("validators", async () => {
-  let networks = store.getState().appConfig;
-  let activeNetwork = networks.networks.find(
-    (element) => element.id === networks.activeNetwork
-  );
+export const loadValidators = createAsyncThunk('validators', async () => {
+  let activeNetwork = store.getState().appConfig.activeNetwork;
   const response = await axios.post(
-    `${activeNetwork?.protocol}://${activeNetwork?.host}:${activeNetwork?.port}/ext/bc/P`,
+    `${activeNetwork?.protocol}://${activeNetwork?.ip}:${activeNetwork?.port}/ext/bc/P`,
     {
-      jsonrpc: "2.0",
-      method: "platform.getCurrentValidators",
+      jsonrpc: '2.0',
+      method: 'platform.getCurrentValidators',
       params: {
         subnetID: null,
         nodeIDs: [],
       },
       id: 1,
-    }
+    },
   );
   return response.data.result.validators;
 });
