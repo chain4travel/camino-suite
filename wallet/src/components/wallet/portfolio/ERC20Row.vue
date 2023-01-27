a119c2ee9ea433df7de0f80a23fa7ecf43b5b3b1
Ayagoumi <agoumi94@gmail.com>
2023-01-27 10:27:36 +0100

    fix add new token

--- wallet/src/components/wallet/portfolio/ERC20Row.vue
+++ wallet/src/components/wallet/portfolio/ERC20Row.vue
@@ -34,7 +34,7 @@ export default class ERC20Row extends Vue {
     }
 
     get sendLink() {
-        return `/wallet/transfer?chain=C&token=${this.token.data.address}`
+        return `/wallet/home/transfer?chain=C&token=${this.token.data.address}`
     }
 }
 </script>
