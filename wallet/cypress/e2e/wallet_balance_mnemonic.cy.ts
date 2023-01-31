ab63a69ca7663c5d9fc0769e0bcd946b619b721d
Esteban Londoño Ospina <stebanlondo75@gmail.com>
2023-01-31 16:32:31 -0500

    Interceptor X Chain

--- wallet/cypress/e2e/wallet_balance_mnemonic.cy.ts
+++ wallet/cypress/e2e/wallet_balance_mnemonic.cy.ts
@@ -13,19 +13,53 @@ describe('Wallet Balance Mnemonic', () => {
     });
 
     after(async () => {
+
+        await interceptXChainBalance();
         cy.get('.header > :nth-child(3) > .v-icon').click();
         await validateAllBalances();
     });
 });
 
 
+async function interceptXChainBalance() {
+    cy.intercept('POST', 'https://columbus.camino.foundation/ext/bc/X', (req) => {
+
+        //Interceptor in X Chain
+        if (req.body.method == "avm.getUTXOs") {
+            req.reply({
+                statusCode: 200,
+                body: {
+                    "jsonrpc": "2.0",
+                    "result": {
+                        "numFetched": "2",
+                        "utxos": [
+                            "0x0000a5c08e35b04842fc5fce925ce09cc9f23023d9f2201447f8c71b9e199884a7eb0000000068c1c17ef684ee4260d1c7ab95fe5222dfd7fa60f3363051dae558072101df9b0000000700000005d21dba0000000000000000000000000100000001fa04f66120aa872cb36b6a43eb3e8aecec46c7d45148a7dd",
+                            "0x0000c118dce645c6004f9562fb6c3b562fda234445c5c961903284b386e4f41923b20000000068c1c17ef684ee4260d1c7ab95fe5222dfd7fa60f3363051dae558072101df9b0000000700000005d21dba0000000000000000000000000100000001fa04f66120aa872cb36b6a43eb3e8aecec46c7d4a577830c"
+                        ],
+                        "endIndex": {
+                            "address": "X-columbus1lgz0vcfq42rjevmtdfp7k052ankyd375puqjpl",
+                            "utxo": "p85phsWY6DpHUnQE4TnGvuJ7LXPPvAv9YmENgWqB5YMfpver4"
+                        },
+                        "encoding": "hex"
+                    },
+                    "id": 40
+                }
+            });
+        }
+        else {
+            console.log("Other query in X Chain");
+        }
+    });
+}
+
+
 async function validateAllBalances() {
     cy.wait(10000);
-    let xFunds : number = await getBalanceText("X");
-    let cFunds : number = await getBalanceText("C");
-    let pFunds : number = await getBalanceText("P");
+    let xFunds: number = await getBalanceText("X");
+    let cFunds: number = await getBalanceText("C");
+    let pFunds: number = await getBalanceText("P");
 
-    let totalFunds : number = await getTotalBalanceText();
+    let totalFunds: number = await getTotalBalanceText();
 
     console.log("fundsData", {
         xFunds: xFunds,
@@ -36,18 +70,17 @@ async function validateAllBalances() {
 
     let comparativeFunds = xFunds + cFunds + pFunds;
 
-    console.log("totalFunds",{
-        totalFundSum : comparativeFunds.toFixed(9),
+    console.log("totalFunds", {
+        totalFundSum: comparativeFunds.toFixed(9),
         totalFundsHTML: totalFunds.toFixed(9)
     });
 
-    if(totalFunds.toFixed(9) != comparativeFunds.toFixed(9))
-    {
+    if (totalFunds.toFixed(9) != comparativeFunds.toFixed(9)) {
         throw new Error("Funds it's not equals");
     }
 }
 
-function getTotalBalanceText() : Promise<number> {
+function getTotalBalanceText(): Promise<number> {
     return new Promise((resolve, reject) => {
         let balanceNumber = "0";
         let balanceDecimals = ".0";
@@ -63,7 +96,7 @@ function getTotalBalanceText() : Promise<number> {
     });
 }
 
-function getBalanceText(chain: string) : Promise<number> {
+function getBalanceText(chain: string): Promise<number> {
     return new Promise((resolve, reject) => {
         let attributeFind = "";
         switch (chain) {
