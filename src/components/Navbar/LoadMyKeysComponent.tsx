b4e0f4ab33ff171a903729ab5505d9b495901406
aeddaqqa <achrafeddaqqaq@gmail.com>
2023-03-29 12:24:58 +0000

    fix resposive modal for alias picker

--- src/components/Navbar/LoadMyKeysComponent.tsx
+++ src/components/Navbar/LoadMyKeysComponent.tsx
@@ -3,6 +3,39 @@ import { mountKyesComponent } from 'wallet/mountKyesComponent'
 import { useAppDispatch } from '../../hooks/reduxHooks'
 import { updateNotificationStatus } from '../../redux/slices/app-config'
 import { useEffectOnce } from '../../hooks/useEffectOnce'
+import { styled } from '@mui/system'
+import { Box } from '@mui/material'
+
+const StyledBox = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    '@media (max-width: 600px)': {
+        justifyContent: 'flex-start',
+        '& .my_keys': {
+            width: '400px',
+        },
+        '& .rows': {
+            flexDirection: 'column !important',
+            justifyContent: 'center !important',
+            alignItems: 'center !important',
+        },
+    },
+    '@media (max-width: 450px)': {
+        justifyContent: 'flex-start',
+        '& .my_keys': {
+            width: '350px',
+        },
+    },
+    '@media (max-width: 400px)': {
+        justifyContent: 'flex-start',
+        '& .my_keys': {
+            width: '200px',
+        },
+    },
+}))
 
 const LoadMyKeysComponent = () => {
     const ref = useRef(null)
@@ -15,9 +48,9 @@ const LoadMyKeysComponent = () => {
     }) // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
-        <>
+        <StyledBox>
             <div ref={ref} />
-        </>
+        </StyledBox>
     )
 }
 
