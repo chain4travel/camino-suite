246549a095acfd72f3228f01dddda2f214ed461e
aeddaqqa <achrafeddaqqaq@gmail.com>
2023-06-20 14:19:02 +0100

    feat: expose the commit hash to the suite

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,23 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+const webpack = require('webpack')
+const { execSync } = require('child_process')
+
+const getGitInformation = () => {
+    try {
+        const commit = execSync('git rev-parse --short HEAD').toString().trim()
+        const repo = execSync('git config --get remote.origin.url').toString().trim()
+        const tag = execSync('git describe --tags --always').toString().trim()
+        return { commit, repo, tag }
+    } catch (e) {
+        return { commit: '', repo: '', tag: '' }
+    }
+}
+
+const gitInfo = getGitInformation()
+
 module.exports = {
     resolve: {
         extensions: ['.vue', '.tsx', '.ts', '.jsx', '.js', '.json'],
@@ -33,6 +50,12 @@ module.exports = {
     },
 
     plugins: [
+        new Dotenv(),
+        new webpack.DefinePlugin({
+            'process.env.GIT_COMMIT_HASH': JSON.stringify(gitInfo.commit),
+            'process.env.VERSION': JSON.stringify(gitInfo.tag),
+            'process.env.RELEASES_PAGE': JSON.stringify(gitInfo.repo),
+        }),
         new HtmlWebPackPlugin({
             template: path.resolve(__dirname, 'public/index.html'),
             favicon: 'public/favicon.ico',
