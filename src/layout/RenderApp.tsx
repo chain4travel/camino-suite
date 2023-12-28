1ea29fbdfe1a704a5557d1861450d9a30d7ed242
Ayoub Agoumi <agoumi94@gmail.com>
2023-12-28 16:29:46 +0100

    refactor: app configuration and routing (#181)
    * refactor: app configuration and routing
    * feat: add loading indicator to when the networks are loading or on network change

--- src/layout/RenderApp.tsx
+++ src/layout/RenderApp.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { useAppSelector } from '../hooks/reduxHooks'
-import { RootState } from '../redux/store'
 import ExplorerApp from './ExplorerApp'
+import React from 'react'
+import { RootState } from '../redux/store'
 import Wallet from './WalletApp'
+import { useAppSelector } from '../hooks/reduxHooks'
 
 const RenderApp = () => {
     const activeApp = useAppSelector((state: RootState) => state.appConfig.activeApp)
-    if (activeApp === 'blockexplorer') return <ExplorerApp />
-    else if (activeApp === 'wallet') return <Wallet />
+    if (activeApp === 'Explorer') return <ExplorerApp />
+    else if (activeApp === 'Wallet') return <Wallet />
     return <div>Not Yet Implemented</div>
 }
 
