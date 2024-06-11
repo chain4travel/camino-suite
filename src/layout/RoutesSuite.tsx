426675b67e16f78ee26eb34bd18634a862ba60c9
aeddaqqa <achrafeddaqqaq@gmail.com>
2024-06-11 13:07:36 +0100

    add dac app to the main view

--- src/layout/RoutesSuite.tsx
+++ src/layout/RoutesSuite.tsx
@@ -24,6 +24,7 @@ import UpgradeCMAccount from '../views/partners/UpgradeCMAccount'
 import MultisigWallet from '../views/settings/MultisigWallet'
 import VerifyWallet from '../views/settings/VerifyWallet'
 import Settings from '../views/settings/index'
+import VoteApp from '../views/vote/VoteApp'
 import Wallet from '../views/wallet/WalletApp'
 import CreateDepositsLayout from './CreateDepositLayout'
 import PartnersLayout from './PartnersLayout'
@@ -97,6 +98,9 @@ export default function RoutesSuite() {
                             path="/explorer"
                             element={<Navigate to={`/explorer/${networkAliasToUrl}`} />}
                         />
+
+                        <Route path={`/dac/*`} element={<VoteApp />} />
+                        <Route path={`/dac`} element={<Navigate to="/dac/active" />} />
                     </>
                 ) : null}
                 <Route element={<Protected />}>
