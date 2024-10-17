7c101ddc02f761a08607597d1e7014f54ded027d
Ysrbolles <ysrbolles@gmail.com>
2024-10-17 13:47:17 +0100

    Refactor rendering logic in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { ExplorerStoreProvider } from 'Explorer/useStore'
 import React from 'react'
-import { hydrate } from 'react-dom'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { Provider as ReduxProvider } from 'react-redux'
@@ -12,34 +11,16 @@ import ThemeConfig from './theme'
 export const store = configureAppStore()
 
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement)
-
-const test = document.getElementById('app')
-if (test.hasChildNodes()) {
-    hydrate(
-        <React.StrictMode>
-            <HelmetProvider>
-                <ExplorerStoreProvider>
-                    <ReduxProvider store={store}>
-                        <ThemeConfig>
-                            <Root />
-                        </ThemeConfig>
-                    </ReduxProvider>
-                </ExplorerStoreProvider>
-            </HelmetProvider>
-        </React.StrictMode>,
-        root,
-    )
-} else
-    root.render(
-        <React.StrictMode>
-            <HelmetProvider>
-                <ExplorerStoreProvider>
-                    <ReduxProvider store={store}>
-                        <ThemeConfig>
-                            <Root />
-                        </ThemeConfig>
-                    </ReduxProvider>
-                </ExplorerStoreProvider>
-            </HelmetProvider>
-        </React.StrictMode>,
-    )
+root.render(
+    <React.StrictMode>
+        <HelmetProvider>
+            <ExplorerStoreProvider>
+                <ReduxProvider store={store}>
+                    <ThemeConfig>
+                        <Root />
+                    </ThemeConfig>
+                </ReduxProvider>
+            </ExplorerStoreProvider>
+        </HelmetProvider>
+    </React.StrictMode>,
+)
