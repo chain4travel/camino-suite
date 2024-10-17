acc50e809adcd01d587f3786e4381655dc03e5b9
aeddaqqa <achrafeddaqqaq@gmail.com>
2024-10-17 16:19:13 +0100

    feat(PartnerConfig): added check to register as partner button

--- src/components/Partners/PartnersFilter.tsx
+++ src/components/Partners/PartnersFilter.tsx
@@ -1,8 +1,12 @@
 import { ActionType, StatePartnersType, partnersActions } from '../../helpers/partnersReducer'
 
 import { Box, Button, Checkbox, FormControlLabel, Typography } from '@mui/material'
-import React from 'react'
+import React, { useMemo } from 'react'
+import store from 'wallet/store'
 import { REGISTER_PARTNER_URL } from '../../constants/route-paths'
+import { useAppSelector } from '../../hooks/reduxHooks'
+import { useIsPartnerQuery } from '../../redux/services/partners'
+import { getActiveNetwork } from '../../redux/slices/network'
 import BusinessFieldFilter from './BusinessFieldFilter'
 import SearchInput from './SearchInput'
 
@@ -14,6 +18,21 @@ interface PartnersFilterProps {
 const PartnersFilter: React.FC<PartnersFilterProps> = ({ state, dispatchPartnersActions }) => {
     const searchByName = param =>
         dispatchPartnersActions({ type: partnersActions.UPDATE_COMPANY_NAME, payload: param })
+
+    const activeNetwork = useAppSelector(getActiveNetwork)
+    const { data } = useIsPartnerQuery({
+        cChainAddress: store?.state?.activeWallet?.ethAddress
+            ? '0x' + store?.state?.activeWallet?.ethAddress
+            : '',
+    })
+    const partnerCChainAddress = useMemo(() => {
+        let cAddress = data?.attributes?.cChainAddresses.find(
+            elem => elem.Network === activeNetwork?.name?.toLowerCase(),
+        )
+        if (cAddress) return cAddress
+        return ''
+    }, [data])
+    const auth = useAppSelector(state => state.appConfig.isAuth)
     return (
         <Box
             sx={{
@@ -76,28 +95,30 @@ const PartnersFilter: React.FC<PartnersFilterProps> = ({ state, dispatchPartners
                     />
                 </Box>
             </Box>
-            <Box
-                sx={{
-                    flex: '1',
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    alignItems: 'flex-start',
-                }}
-            >
-                <Button
-                    variant="contained"
-                    component="a"
-                    href={REGISTER_PARTNER_URL}
-                    target="_blank"
-                    rel="noopener noreferrer"
+            {(!auth || !partnerCChainAddress) && (
+                <Box
                     sx={{
-                        whiteSpace: 'nowrap',
-                        minWidth: 'max-content',
+                        flex: '1',
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        alignItems: 'flex-start',
                     }}
                 >
-                    Register as Partner
-                </Button>
-            </Box>
+                    <Button
+                        variant="contained"
+                        component="a"
+                        href={REGISTER_PARTNER_URL}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        sx={{
+                            whiteSpace: 'nowrap',
+                            minWidth: 'max-content',
+                        }}
+                    >
+                        Register as Partner
+                    </Button>
+                </Box>
+            )}
         </Box>
     )
 }
