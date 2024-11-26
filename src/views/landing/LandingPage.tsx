6fa5d235d66c81d0d1577cf4f97d7b86def53c7b
Ysrbolles <ysrbolles@gmail.com>
2024-11-26 14:34:35 +0100

    chore: Remove setTimeout from feature check in LandingPage component

--- src/views/landing/LandingPage.tsx
+++ src/views/landing/LandingPage.tsx
@@ -20,9 +20,7 @@ export default function LandingPage() {
     const { getUpgradePhases } = useWallet()
 
     useEffect(() => {
-        setTimeout(() => {
-            checkFeature()
-        }, 1000)
+        checkFeature()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [activeNetwork])
 
