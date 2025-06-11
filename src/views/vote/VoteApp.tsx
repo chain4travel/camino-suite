17cf5078133a3ba54b76e26383b4254fc12f22c2
Achraf Eddaqqaq <51858084+aeddaqqa@users.noreply.github.com>
2025-06-11 15:55:25 +0100

    fix: update voting app when switching the wallet (#317) (#318)

--- src/views/vote/VoteApp.tsx
+++ src/views/vote/VoteApp.tsx
@@ -1,15 +1,23 @@
 import { useTheme } from '@mui/system'
 import React from 'react'
+import { useAppSelector } from '../../hooks/reduxHooks'
 import useNetwork from '../../hooks/useNetwork'
+import { getPChainAddress } from '../../redux/slices/app-config'
 const Vote = React.lazy(() => import('DAC/dac'))
 
 const VoteApp = () => {
     const { activeNetwork } = useNetwork()
+    const pChainAddress = useAppSelector(getPChainAddress)
     const theme = useTheme()
 
     return (
         <React.Suspense fallback={<div>Loading...</div>}>
-            <Vote key={activeNetwork?.id} network={activeNetwork} theme={theme} />
+            <Vote
+                key={activeNetwork?.id}
+                network={activeNetwork}
+                pChainAddress={pChainAddress}
+                theme={theme}
+            />
         </React.Suspense>
     )
 }
