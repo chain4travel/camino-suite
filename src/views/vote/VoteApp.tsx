7ebea35f5811643302c4ed8d37e4de95d171f74f
aeddaqqa <achrafeddaqqaq@gmail.com>
2025-07-25 10:05:24 +0100

    feat: add notifications component to the voting app

--- src/views/vote/VoteApp.tsx
+++ src/views/vote/VoteApp.tsx
@@ -1,13 +1,17 @@
 import { useTheme } from '@mui/system'
 import React from 'react'
-import { useAppSelector } from '../../hooks/reduxHooks'
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks'
 import useNetwork from '../../hooks/useNetwork'
-import { getPChainAddress } from '../../redux/slices/app-config'
+import { getPChainAddress, updateNotificationStatus } from '../../redux/slices/app-config'
 const Vote = React.lazy(() => import('DAC/dac'))
 
 const VoteApp = () => {
     const { activeNetwork } = useNetwork()
+    const dispatch = useAppDispatch()
     const pChainAddress = useAppSelector(getPChainAddress)
+    const dispatchNotification = ({ message, type }) => {
+        dispatch(updateNotificationStatus({ message, severity: type }))
+    }
     const theme = useTheme()
 
     return (
@@ -17,6 +21,7 @@ const VoteApp = () => {
                 network={activeNetwork}
                 pChainAddress={pChainAddress}
                 theme={theme}
+                dispatchNotification={dispatchNotification}
             />
         </React.Suspense>
     )
