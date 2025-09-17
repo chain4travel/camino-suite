4b5400477b9e1300826c4329ec285a73df9e6621
Ekrem Seren <ekrem.seren@gmail.com>
2025-09-17 07:45:34 +0300

    fix merge

--- src/views/vote/VoteApp.tsx
+++ src/views/vote/VoteApp.tsx
@@ -1,27 +1,17 @@
 import { useTheme } from '@mui/system'
 import React from 'react'
-<<<<<<< HEAD
-import { useAppSelector } from '../../hooks/reduxHooks'
-import useNetwork from '../../hooks/useNetwork'
-import { getPChainAddress } from '../../redux/slices/app-config'
-=======
 import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks'
 import useNetwork from '../../hooks/useNetwork'
 import { getPChainAddress, updateNotificationStatus } from '../../redux/slices/app-config'
->>>>>>> suite
 const Vote = React.lazy(() => import('DAC/dac'))
 
 const VoteApp = () => {
     const { activeNetwork } = useNetwork()
-<<<<<<< HEAD
-    const pChainAddress = useAppSelector(getPChainAddress)
-=======
     const dispatch = useAppDispatch()
     const pChainAddress = useAppSelector(getPChainAddress)
     const dispatchNotification = ({ message, type }) => {
         dispatch(updateNotificationStatus({ message, severity: type }))
     }
->>>>>>> suite
     const theme = useTheme()
 
     return (
@@ -31,10 +21,7 @@ const VoteApp = () => {
                 network={activeNetwork}
                 pChainAddress={pChainAddress}
                 theme={theme}
-<<<<<<< HEAD
-=======
                 dispatchNotification={dispatchNotification}
->>>>>>> suite
             />
         </React.Suspense>
     )
